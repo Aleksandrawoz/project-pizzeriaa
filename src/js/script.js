e70bb8f0a59028d730655fde599a5ea4eada9350
ola wozniak <olawozniak551@gmail.com>
2021-09-22 13:01:02 +0200

    value validate

--- src/js/script.js
+++ src/js/script.js
@@ -59,6 +59,58 @@
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML)
   };
+  class AmountWidget {
+    constructor(element) {
+      const thisWidget = this;
+      thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
+      console.log('AmountWidget:', thisWidget);
+      console.log('constructor arguments:', element);
+    }
+    getElements(element) {
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+    setValue(value) {
+      const thisWidget = this;
+
+      const newValue = parseInt(value);
+      /*TODO: Add validation */
+      console.log(settings.amountWidget.defaultMin);
+      console.log(settings.amountWidget.defaultMax);
+      if (thisWidget.value !== newValue && !isNaN(newValue) && newValue >= (settings.amountWidget.defaultMin - 1) && newValue <= (settings.amountWidget.defaultMax + 1)) {
+        thisWidget.value = newValue;
+        thisWidget.announce();
+      }
+      thisWidget.input.value = thisWidget.value;
+    }
+
+    initActions() {
+      const thisWidget = this;
+      thisWidget.input.addEventListener('change', function () {
+        thisWidget.setValue(thisWidget.input.value);
+      });
+      thisWidget.linkDecrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+      thisWidget.linkIncrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+    }
+    announce() {
+      const thisWidget = this;
+
+      const event = new Event('updated');
+      thisWidget.element.dispatchEvent(event);
+    }
+  }
   const app = {
     initMenu: function () {
       const thisApp = this;
@@ -93,6 +145,7 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
       console.log('new Product:', thisProduct);
     }
@@ -117,8 +170,17 @@
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
     }
 
+    initAmountWidget() {
+      const thisProduct = this;
+
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+      thisProduct.amountWidgetElem.addEventListener('updated', function () {
+        thisProduct.processOrder();
+      });
+    }
 
     initAccordion() {
       const thisProduct = this;
@@ -151,14 +213,13 @@
       thisProduct.form.addEventListener('submit', function (event) {
         event.preventDefault();
         thisProduct.processOrder();
-      }); 
+      });
 
       for (let input of thisProduct.formInputs) {
         input.addEventListener('change', function () {
           thisProduct.processOrder();
         });
       }
-
       thisProduct.cartButton.addEventListener('click', function (event) {
         event.preventDefault();
         thisProduct.addToCart();
@@ -204,25 +265,25 @@
               price -= option.price;
             }
           }
-              const optionImage = thisProduct.imageWrapper.querySelector('.' + paramID + '-' + optionID);
-              if(optionImage){
-                //yes! we've found it!
-                if(optionSelected){
-                  optionImage.classList.add(classNames.menuProduct.imageVisible);
-                }
-                else{
-                  optionImage.classList.remove(classNames.menuProduct.imageVisible);
-                }
-              }
-    
-              // update calculated price in the HTML
-
-              thisProduct.priceElem.innerHTML = price;
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramID + '-' + optionID);
+          if (optionImage) {
+            //yes! we've found it!
+            if (optionSelected) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            }
+            else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
             }
           }
+          /*multiply price by amount*/
+          price *= thisProduct.amountWidget.value;
+          // update calculated price in the HTML
+          thisProduct.priceElem.innerHTML = price;
         }
       }
-    
-  
+    }
+  }
+
+
   app.init();
 }
