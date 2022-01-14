0bcfa06fa726812aa81c88d88b22ffa2d958608d
ola wozniak <olawozniak551@gmail.com>
2022-01-14 01:51:41 +0100

    correct

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -20,7 +20,7 @@ class HourPicker extends BaseWidget{
     thisWidget.dom.input.addEventListener('input', function(){
       thisWidget.value = thisWidget.dom.input.value;
     });
-  }}
+  }
 
   parseValue(value){
     return utils.numberToHour(value);
